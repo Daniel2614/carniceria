9ddaeef248e82d3522889d808c87d039d5c4c8fe
Daniel2614 <dperezry@gmail.com>
2019-03-15 11:05:33 -0600

    vista principal

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('evaluacion', function () {
+    return view('evaluacion');
 });
